a2291de63808de990a2a4d9ec23d7ac23a20ec12
caozhiquan <quancando@126.com>
2017-08-01 13:21:47 +0800

    Initial Commit

---
+++ trial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  trial
+//
+//  Created by chuangke-15 on 2017/8/1.
+//  Copyright © 2017年 makeschool. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
